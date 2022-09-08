2cf8ca7c3eb8d99307612393d058173d6af10be4
Chaika <chaika1986@mail.ru>
2022-09-08 22:59:50 +0700

    all files

--- src/main/java/NegativeIdException.java
+++ src/main/java/NegativeIdException.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class NegativeIdException {
+public class NegativeIdException extends RuntimeException {
+
+    public NegativeIdException(String msg) {
+        super(msg);
+    }
 }
